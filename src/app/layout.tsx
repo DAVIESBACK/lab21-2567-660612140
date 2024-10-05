a69ceb55db71bfa40544331ee51c2df67a526038
win <wwinnn478@gmail.com>
2024-10-05 17:47:28 +0700

    TNT

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { Container, Group, Loader, Title } from "@mantine/core";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState, useTransition } from "react";
 import Footer from "@components/Footer";
-import axios from "axios";
 
 import { Inter } from "next/font/google";
 import { MantineProvider } from "@mantine/core";
@@ -32,16 +31,7 @@ export default function RootLayout({
     if (!token || !authenUsername) {
       isTokenValid = false;
     } else {
-      //check if token is still valid
-      try {
-        const resp = await axios.get("/api/user/checkAuthen", {
-          headers: { Authorization: `Bearer ${token}` },
-        });
         $authenStore.set({ token, authenUsername });
-      } catch (err) {
-        console.log(err.message);
-        isTokenValid = false;
-      }
     }
 
     //go to login if not logged in yet and trying to access protected route
